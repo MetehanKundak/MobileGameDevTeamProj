828e6f5d97ca5269036faba48665f88748cd2863
Mertt Doğanay <MERTLOL3@outlook.com>
2021-12-07 23:04:13 -0500

    New method GameStart created in the GameStart.java class, We are starting the initial activity with our main class which is MainActivity.

--- app/src/main/java/com/example/mobilegamedevteamproj/GameStart.java
+++ app/src/main/java/com/example/mobilegamedevteamproj/GameStart.java
@@ -1,7 +1,9 @@
 package com.example.mobilegamedevteamproj;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,11 @@ public class GameStart extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_start);
+    }
+
+    public void GameStart(View view){
+
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
